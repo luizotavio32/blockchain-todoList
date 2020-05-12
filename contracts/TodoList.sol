1f81106732cd773df75d546a7f98a130356372f9
lopasso <luiz.passos@sonda.com>
2020-05-12 07:55:53 -0400

    Second commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,23 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskcount = 0; //estado do contato
-}
+    uint public taskCount = 0; //estado do contato
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks; //id da task
+
+    constructor() public {
+        createTask("Inserindo Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+
+    }
+}
