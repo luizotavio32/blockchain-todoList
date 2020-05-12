859a48ffb68d007641590ab3bee9d9e87d00d650
lopasso <luiz.passos@sonda.com>
2020-05-12 15:14:33 -0400

    Checking tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -13,6 +13,8 @@ contract TodoList {
 
     event TaskCreated(uint id,string content,bool completed);
 
+    event TaskCompleted(uint id,bool completed);
+
     constructor() public {
         createTask("Inserindo Task");
     }
@@ -21,5 +23,12 @@ contract TodoList {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
